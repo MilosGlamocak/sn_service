724540d7abd6391820bbf8f3efb651cd7e0ce82f
MilosGlamocak <milosglamocak@gmail.com>
2024-03-28 18:59:16 +0100

    More styling updates

--- src/Components/WelcomePage.jsx
+++ src/Components/WelcomePage.jsx
@@ -9,8 +9,9 @@ function WelcomePage() {
         <Container className="welcomeContainer">
             <h1 className="welcomeHeader">Autoservis Banja Luka</h1>
             <p className="welcomeText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis praesentium reprehenderit alias nulla nisi fugit inventore nobis debitis blanditiis eaque sunt laborum omnis accusantium asperiores, ratione dolor ipsa? Nemo, facilis?</p>
-            <Container className="welcomeContainerBg"></Container> 
+            
          </Container>
+         <Container className="welcomeContainerBg"></Container> 
         <Button className="scrollButton" variant="contained">
             <ArrowDownwardIcon className="icon arrowIcon"/>
         </Button>
