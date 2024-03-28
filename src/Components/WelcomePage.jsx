274172929801e95ffa2afa44d896593676226bbf
MilosGlamocak <milosglamocak@gmail.com>
2024-03-28 17:33:08 +0100

    Added background image and scrollDownButton

---
+++ src/Components/WelcomePage.jsx
@@ -0,0 +1,17 @@
+import { Button, Container } from "@mui/material"
+import '../Styles/WelcomePage.css'
+
+function WelcomePage() {
+
+    return (
+        <>
+        <Container className="welcomeContainer">
+            
+            </Container>
+            <Button className="scrollButton" variant="contained">Scroll</Button>
+        </>
+        
+    )
+}
+
+export default WelcomePage
