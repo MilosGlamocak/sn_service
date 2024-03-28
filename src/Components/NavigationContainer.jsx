db6a920fdecfc4df9f77bee22d90510a9fbb78c5
MilosGlamocak <milosglamocak@gmail.com>
2024-03-28 13:50:25 +0100

    Imported MUI

--- src/Components/NavigationContainer.jsx
+++ src/Components/NavigationContainer.jsx
@@ -1,5 +1,13 @@
+import { Container } from "@mui/material";
+import React from "react";
+
+
+
 function NavigationContainer() {
-  console.log(true);
+  return (
+    
+    <Container style={{height: '80px', backgroundColor: 'red', width: '100%'}}></Container>
+  )
 }
 
 export default NavigationContainer;
