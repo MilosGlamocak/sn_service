c5124ede07ab7c4fde3b6aafbe8b6352c9fa2d67
MilosGlamocak <milosglamocak@gmail.com>
2024-03-28 18:50:43 +0100

    Added mockup text + container for text

--- src/Components/WelcomePage.jsx
+++ src/Components/WelcomePage.jsx
@@ -7,7 +7,9 @@ function WelcomePage() {
     return (
         <>
         <Container className="welcomeContainer">
-            
+            <h1 className="welcomeHeader">Autoservis Banja Luka</h1>
+            <p className="welcomeText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis praesentium reprehenderit alias nulla nisi fugit inventore nobis debitis blanditiis eaque sunt laborum omnis accusantium asperiores, ratione dolor ipsa? Nemo, facilis?</p>
+            <Container className="welcomeContainerBg"></Container> 
          </Container>
         <Button className="scrollButton" variant="contained">
             <ArrowDownwardIcon className="icon arrowIcon"/>
