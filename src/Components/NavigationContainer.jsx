884780abcad84316aeb2a96292c073111bd44d1e
MilosGlamocak <milosglamocak@gmail.com>
2024-03-28 14:06:22 +0100

    Updated topNav style

--- src/Components/NavigationContainer.jsx
+++ src/Components/NavigationContainer.jsx
@@ -2,13 +2,15 @@ import { Container } from "@mui/material";
 import React from "react";
 import '../Styles/NavigationContainer.css'
 
-
+//treba ponuda, kontakt, lokacija, tako nesto u top nav
 
 function NavigationContainer() {
   return (
     
     <Container className="topNav">
+        <Container className=" nav leftNav"></Container>
         <h1 className="siteName">SN Service</h1>
+        <Container className="nav rightNav"></Container>
     </Container>
   )
 }
