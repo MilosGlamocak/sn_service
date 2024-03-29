6499a13e3cf8bada173a46ca2093a94a0423d391
MilosGlamocak <milosglamocak@gmail.com>
2024-03-29 13:34:59 +0100

    Added more styles for placeholderComponents

--- src/Components/InfoPage.jsx
+++ src/Components/InfoPage.jsx
@@ -1,12 +1,18 @@
 import React from "react"
 import '../Styles/InfoPage.css'
 import { Container } from "@mui/material"
+import CarRepairIcon from '@mui/icons-material/CarRepair';
 
 
 function InfoPage() {
 return (
     <Container className="infoContainer">
-        <Container className="infoIcons"></Container>
+        
+        <Container className="infoIcons">
+            <CarRepairIcon className="infoIcon carRepairIcon"/>
+        </Container>
+        <Container className="infoIconText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, repellendus.</Container>
+        <Container className="infoMechanics"></Container>
     </Container>
 )
 }
