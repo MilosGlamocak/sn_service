9ece0f6519fe77b388b5a8ffc0cd82a44a539fc6
MilosGlamocak <milosglamocak@gmail.com>
2024-03-29 13:09:22 +0100

    Created new component placeholder

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Container } from '@mui/material';
 import './App.css';
 import NavigationContainer from './Components/NavigationContainer';
 import WelcomePage from './Components/WelcomePage';
+import InfoPage from './Components/InfoPage';
 
 function App() {
   return (
     <Container className='container'>
       <NavigationContainer />
       <WelcomePage />
+      <InfoPage />
     </Container>
     
   );
---
+++ src/Components/InfoPage.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import '../Styles/InfoPage.css'
+import { Container } from "@mui/material"
+
+
+function InfoPage() {
+return (
+    <Container className="infoContainer">
+        <Container className="infoIcons"></Container>
+    </Container>
+)
+}
+
+export default InfoPage
