ed6be8a75d78a129b4e8c46de7a8ae36d8f95f79
MilosGlamocak <milosglamocak@gmail.com>
2024-03-29 14:16:39 +0100

    More styling updates for infoPage
    Added 4 MUI icons and 4 text placeholders + placeholder for mechanics info

--- src/Components/InfoPage.jsx
+++ src/Components/InfoPage.jsx
@@ -2,16 +2,31 @@ import React from "react"
 import '../Styles/InfoPage.css'
 import { Container } from "@mui/material"
 import CarRepairIcon from '@mui/icons-material/CarRepair';
+import BuildIcon from '@mui/icons-material/Build';
+import MiscellaneousServicesIcon from '@mui/icons-material/MiscellaneousServices';
+import BoltIcon from '@mui/icons-material/Bolt';
 
 
 function InfoPage() {
+
+    const infoIconText = [{
+        className: 'infoIconText',
+        value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, repellendus.'
+    }]
 return (
     <Container className="infoContainer">
         
         <Container className="infoIcons">
             <CarRepairIcon className="infoIcon carRepairIcon"/>
+            <BuildIcon className="infoIcon buildIcon"/>
+            <MiscellaneousServicesIcon className="infoIcon miscIcon" />
+            <BoltIcon className="infoIcon boltIcon" />
         </Container>
-        <Container className="infoIconText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, repellendus.</Container>
+        <Container className="infoIconText">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p></Container>
         <Container className="infoMechanics"></Container>
     </Container>
 )
