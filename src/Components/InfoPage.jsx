58a380f875a4bc65b9507db5e0dca6a0475f3af4
MilosGlamocak <milosglamocak@gmail.com>
2024-03-30 15:48:12 +0100

    Hover now works as intended
    Next step should be adding a border glow

--- src/Components/InfoPage.jsx
+++ src/Components/InfoPage.jsx
@@ -8,46 +8,55 @@ import BoltIcon from '@mui/icons-material/Bolt';
 
 
 function InfoPage() {
+    const [hoveredCard, setHoveredCard] = useState(null);
 
-    const [mousePosition, setMousePosition] = useState({x: 0, y: 0});
+    const handleOnMouseMove = (index, e) => {
+        const { currentTarget: target } = e;
+        const rect = target.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
 
-    const handleOnMouseMove = (e) => {
-        const {currentTarget: target} = e;
-
-        const rect = target.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-
-        setMousePosition({x, y})
-    }
+        setHoveredCard({ index, x, y });
+    };
 
 return (
-    <>
+<>
     <Container className="infoContainer">
-    <Grid container columns={{ xs: 3, sm: 2, md: 40 }}
-    columnGap={{xs: 0, sm: 0, md: 24}}
-    rowGap={{xs: 1, md: 8}} className="infoGrid">
+        <Grid
+            container
+            columns={{ xs: 3, sm: 2, md: 40 }}
+            columnGap={{xs: 0, sm: 0, md: 24}}
+            rowGap={{xs: 1, md: 8}}
+            className="infoGrid"
+        >
         {Array.from(Array(6)).map((_, index) => (
-          <Grid item xs={1} sm={1} md={7} key={index} >
-            <Container className='infoDiv' onMouseMove={handleOnMouseMove} style={{"--mouse-x": `${mousePosition.x}px`,
-                        "--mouse-y": `${mousePosition.y}px`}}>
-                <CarRepairIcon className="infoIcon carRepairIcon"/>
-                <p className="infoIconText">Redovni servisi</p>
-            </Container>
-          </Grid>
-        ))}
-      </Grid>
+            <Grid item xs={1} sm={1} md={7} key={index}>
+                <Container
+                    className='infoDiv'
+                    onMouseMove={(e) => handleOnMouseMove(index, e)}
+                        style={
+                        hoveredCard && hoveredCard.index === index ? {
+                            "--mouse-x": `${hoveredCard.x}px`,
+                            "--mouse-y": `${hoveredCard.y}px`
+                        }
+                        : {}
+                    }
+                    >
+                        <CarRepairIcon className="infoIcon carRepairIcon"/>
+                        <p className="infoIconText">Redovni servisi</p>
+                </Container>
+            </Grid>
+            ))}
+        </Grid>
     </Container>
     <Container className="infoMechanics">
-        <Container className="mechanicText"></Container>
-        <Container className="mechanicImg"></Container>
+        {/* Add your other components here */}
     </Container>
-    </>
-    
-)
+</>
+);
 }
 
-export default InfoPage
+export default InfoPage;
 
 /**<Container className="infoIcons">
             <CarRepairIcon className="infoIcon carRepairIcon"/>
