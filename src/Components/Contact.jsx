03bdd3364ac07e5353e06fab7005504c20154116
MilosGlamocak <milosglamocak@gmail.com>
2024-04-02 19:35:06 +0200

    Added text fields for email input

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,4 +1,5 @@
-import { Container } from '@mui/material'
+import { Container, TextField, colors } from '@mui/material'
+import { inputLabelClasses } from "@mui/material/InputLabel";
 import { useState } from 'react';
 import '../Styles/Contact.css'
 
@@ -14,8 +15,20 @@ function Contact() {
 
         setHoveredCard({x, y });
     };
+
+    function handleSendEmail(e) {
+        console.log(e)
+    }
+
+    const inputFields = [
+        {label: 'Vaš email:', className: 'textField emailInput'},
+        {label: 'Poruka:', className: 'textField MessageInput'}, 
+    ]
+
+
     return (
         <Container className='contactContainer'>
+            <h2 className='contactHeader'>Kontaktirajte nas!</h2>
             <Container
                     className='form'
                     onMouseMove={(e) => handleOnMouseMove( e)}
@@ -29,6 +42,22 @@ function Contact() {
                     >
                         <Container className="formBorder"></Container>
                         <Container className={`formContent`}>
+                            {inputFields.map((field) => {
+                                return (
+                            <TextField className={field.className} variant='filled' label={field.label}
+                            sx={{ input: { color: 'white'} }}
+                            InputLabelProps={{
+                                sx: {
+                                  color: "white",
+                                  [`&.${inputLabelClasses.shrink}`]: {
+                                    // set the color of the label when shrinked (usually when the TextField is focused)
+                                    color: "white"
+                                  }}}}
+                            />
+                            )
+                            
+                            })}
+                            
                         </Container>
 
                         
